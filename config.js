b1b8211180def90607f23917704c6cd80eeba379
Geemkuu <144843941+Geemkuu@users.noreply.github.com>
2024-02-06 21:31:23 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,14 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['918360234087', 'WHITE', true], 
-  ['233208179431', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
-  ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
-  ['233533763772', 'Abraham Dwamena2', true],
-  ['233531910067', 'Abraham Dwamena3', true],
+  ['254700569709', '亗『ＧＥＥ』亗', true], 
+  ['254794286079', '亗『ＧＥＥ』亗', true], 
+  
+
   
 ] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
+//global.pairingNumber = "254700569709" //put your bot number here
 
 global.mods = ['923184070915', '94789481495', '923184474176','233531910067','233208179431'] 
 global.prems = ['923184070915','233208179431','94789481495', '918360234087','233531910067']
@@ -43,6 +41,7 @@ global.APIs = { // API Prefix
   nrtm: 'https://fg-nrtm.ddns.net',
   bg: 'http://bochil.ddns.net',
   fgmods: 'https://api-fgmods.ddns.net'
+  chatgpt:'https://chat.openai.com'
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
@@ -52,27 +51,28 @@ global.APIKeys = { // APIKey Here
   'https://violetics.pw': 'beta',
   'https://zenzapis.xyz': `${keysxxx}`, 
   'https://api-fgmods.ddns.net': 'fg-dylux'
+  'https://chat.openai.com':'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'
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = 'n͜͡önesence ×'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
-global.author = '@XLICON-V2' 
+global.packname = 'n͜͡önesence ×' 
+global.author = '亗『ＧＥＥ』亗' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
 global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
-global.fgsc = 'https://github.com/salmanytofficial/XLICON-V2-MD' 
+global.fgsc = 'https://github.com/Geemkuu/nonesence' 
 global.fgyt = 'https://youtube.com/@s4salmanyt'
 global.fgpyp = 'https://youtube.com/@s4salmanyt'
 global.fglog = 'XLICON.jpg' 
 global.thumb = fs.readFileSync('./XLICON.jpg')
 
 
-global.wait = '*🕣 _XLICON IS LODIND..._*\n*▰▰▰▱▱▱▱▱⭐*'
+global.wait = '*🕣 _n͜͡önesence × IS LOADING..._*\n*▰▰▰▱▱▱▱▱⭐*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
-global.done = '✔️'
+global.done = '🚮'
 global.error = '❌' 
 global.xmoji = '🔥' 
 
